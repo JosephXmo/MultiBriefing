80aa6d03ed19e14514854b61605b7a8cd3ecc9be
Joseph Phantom <34392137+JosephXmo@users.noreply.github.com>
2024-09-09 13:19:52 +0800

    Update MSServer.cpp
    Added license prompt function.

--- Server/MSServer.cpp
+++ Server/MSServer.cpp
@@ -1,4 +1,24 @@
-﻿#include "MultiBriefingServer.hpp"
+/*
+     MultiBriefing Communicator - An experimental program implementing chat over IP, and more.
+     Copyright (C) 2024  Sibo Qiu, Runjie Miao, Yucheng Tong, and ? ?
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+     Please contact dev team via qhdqsb@hotmail.com if you need to.
+ */
+
+#include "MultiBriefingServer.hpp"
 
 SOCKET ListenSocket = INVALID_SOCKET;
 
@@ -6,6 +26,8 @@ bool LoopProceedFlag = true;
 
 int __cdecl MSServer(void)
 {
+	firstLicensePrompt();
+	
 	WSADATA wsaData;
 	int iResult;
 
@@ -247,4 +269,9 @@ void Communication(MBClient* Client) {
 	sprintf_s(AnnounceMsg, "%s left this chat.\n", tempname);
 	Announcement(&ClientsTable, AnnounceMsg);
 	ReportClientCounter(ClientsTable.counter);
+}
+
+void firstLicensePrompt() {
+    const char info[290] = "\n\tMultiBriefing Communicators  Copyright (C) 2024  Sibo Qiu, Runjie Miao, Yucheng Tong, and ? ?\n\tThis program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n\tThis is free software, and you are welcome to redistribute it\n\tunder certain conditions; type `show c' for details.\n";
+    std::cout << info << std::endl;
 }
